55b92935c56236975bc9af1cff42a89b7c60c382
Daeuning <happyjde3@gmail.com>
2025-02-17 17:06:07 +0900

    feat: client main 페이지 및 라우팅 설정

---
+++ client/src/pages/main.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+  height: 100vh;
+  width: 100%;
+`;
+
+function Main() {
+  return (
+    <Container>
+    </Container>
+  );
+}
+
+export default Main;
