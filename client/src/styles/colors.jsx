9a35091743ece4eb869bb11190c74e20b4c8b286
Daeuning <happyjde3@gmail.com>
2025-02-18 12:13:07 +0900

    design: color 변수 disable grey 추가

--- client/src/styles/colors.jsx
+++ client/src/styles/colors.jsx
@@ -2,7 +2,8 @@ export const COLORS = {
   black_font: '#373D47',
   dark_grey_font: '#495057',
   grey_font: '#8391A1',
-  
+
   light_grey: '#D9D9D9',
+  disablee_grey: '#9E9E9F',
   purple: '#AA89DB'
 }
